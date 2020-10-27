2eb6cced24671dc6abad01094b4088b201f82db5
zazazh <zazahikayat@gmail.com>
2020-10-27 14:21:49 +0700

    frist-commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## INSTALATION 
+
+- git clone https://github.com/hikayatz/yii2-basic-template.git
+- cd yii2-basic-template
+- composer install
+
+## RUN APP
+
+- php -S localhost:8000
+- run app test => localhost:8000/index-test.php
+
---
+++ app/asset_manager/AppAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\asset_manager;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'resources/css/site.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
